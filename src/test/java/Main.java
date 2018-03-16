040d6fd3aa88c89251bc547cf6c15f821c148394
Sasha Sasha <aasutyagina@gmail.com>
2018-03-16 19:06:15 +0300

    version 2.0 s Please enter the commit message for your changes. Lines starting

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -9,23 +9,28 @@ import java.io.IOException;
 
 public class Main {
 
-    @Test
-    public void main() {
+    public static HttpResponse GetSuggests(String query, int limit, String lang, String locale, String currency) throws IOException {
+        HttpResponse response = Request.Get(
+            String.format("https://www.onetwotrip.com/_hotels/api/suggestRequest?query=%s&limit=%d&lang=%s&locale=%s&currency=%s", query, limit, lang, locale, currency))
+            .execute()
+            .returnResponse();
 
+        return response;
+    }
+
+    @Test
+    public void testSuggestsPositive() {
         try {
 
-            HttpResponse response = Request.Get(
-                    "https://www.onetwotrip.com/_hotels/api/suggestRequest?query=moscow&limit=7&lang=ru&locale=ru&currency=RUB")
-                    .execute()
-                    .returnResponse();
+            HttpResponse response = GetSuggests("moscow", 7, "ru", "ru", "RUB"); //делаем запрос
+            String json = EntityUtils.toString(response.getEntity()); //получаем body из респонса и кладем в строку
+
+            //парсим json
+            ObjectMapper mapper = new ObjectMapper();
+            ApiResponseBody apiResponseBody = mapper.readValue(json, ApiResponseBody.class);
 
             Assert.assertEquals("status code is not 200", 200, response.getStatusLine().getStatusCode());
             System.out.println("status code is 200");
-
-            String json = EntityUtils.toString(response.getEntity());
-
-            ObjectMapper mapper = new ObjectMapper();
-            ApiResponseBody apiResponseBody = mapper.readValue(json, ApiResponseBody.class);
 
             Assert.assertEquals("error is not null", null, apiResponseBody.error);
             System.out.println("error is null");
@@ -34,8 +39,7 @@ public class Main {
             System.out.println("result is not null");
 
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch (IOException ex) {
             Assert.fail();
         }
     }
